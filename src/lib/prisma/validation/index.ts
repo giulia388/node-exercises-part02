596c460518d842704fe7d63c10e0d031a9fb0aff
giulia388 <giuliacolucci388@gmail.com>
2022-10-10 22:24:46 +0200

    Refactor your application

--- src/lib/prisma/validation/index.ts
+++ src/lib/prisma/validation/index.ts
@@ -2,7 +2,9 @@ import addFormats from "ajv-formats";
 import { Validator, ValidationError } from "express-json-validator-middleware";
 import { ErrorRequestHandler } from "express";
 
-const validator = new Validator({});
+const validator = new Validator({
+    coerceTypes: true,
+});
 
 addFormats(validator.ajv, ["date-time"])
     .addKeyword("kind")
@@ -10,10 +12,15 @@ addFormats(validator.ajv, ["date-time"])
 
 export const validate = validator.validate;
 
-export const validationErrorMiddleware: ErrorRequestHandler = (error, request, response, next) => {
+export const validationErrorMiddleware: ErrorRequestHandler = (
+    error,
+    request,
+    response,
+    next
+) => {
     if (error instanceof ValidationError) {
         response.status(422).send({
-            errors: error.validationErrors
+            errors: error.validationErrors,
         });
 
         next();
