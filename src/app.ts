321c6ca7bfc57bfb0cfa426402a30476fe800e60
giulia388 <giuliacolucci388@gmail.com>
2022-10-10 22:41:25 +0200

    implement authentication

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,24 @@ import "express-async-errors";
 
 import { validationErrorMiddleware } from "./lib/prisma/validation";
 import { initCorsMiddleware } from "./lib/prisma/middleware/cors";
+import { initSessionMiddleware } from "./lib/prisma/middleware/session";
+import { passport } from "./lib/prisma/middleware/passport";
 
 import planetsRoutes from "./routes/planets";
+import authRoutes from "./routes/authentication";
 
 const app = express();
+
+app.use(initSessionMiddleware());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 
 app.use(initCorsMiddleware());
 
 app.use("/planets", planetsRoutes);
+app.use("/auth", authRoutes);
 
 app.use(validationErrorMiddleware);
 
